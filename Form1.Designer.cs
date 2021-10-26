91680b4ab1764f66b130e59dafc8f8c8cd3b3639
Tyler Goodfred <tmgoodfred@gmail.com>
2021-10-26 13:51:48 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿
+namespace DNDCharaRandomizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.classTxt = new System.Windows.Forms.TextBox();
+            this.strTxt = new System.Windows.Forms.TextBox();
+            this.dexTxt = new System.Windows.Forms.TextBox();
+            this.conTxt = new System.Windows.Forms.TextBox();
+            this.intTxt = new System.Windows.Forms.TextBox();
+            this.wisTxt = new System.Windows.Forms.TextBox();
+            this.charTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.randomClassBtn = new System.Windows.Forms.Button();
+            this.randomStatsBtn = new System.Windows.Forms.Button();
+            this.raceTxt = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.subRaceTxt = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.featOrFlawTxt = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // classTxt
+            // 
+            this.classTxt.Location = new System.Drawing.Point(111, 101);
+            this.classTxt.Name = "classTxt";
+            this.classTxt.Size = new System.Drawing.Size(159, 20);
+            this.classTxt.TabIndex = 0;
+            // 
+            // strTxt
+            // 
+            this.strTxt.Location = new System.Drawing.Point(352, 51);
+            this.strTxt.Name = "strTxt";
+            this.strTxt.Size = new System.Drawing.Size(40, 20);
+            this.strTxt.TabIndex = 2;
+            // 
+            // dexTxt
+            // 
+            this.dexTxt.Location = new System.Drawing.Point(407, 51);
+            this.dexTxt.Name = "dexTxt";
+            this.dexTxt.Size = new System.Drawing.Size(40, 20);
+            this.dexTxt.TabIndex = 3;
+            // 
+            // conTxt
+            // 
+            this.conTxt.Location = new System.Drawing.Point(471, 51);
+            this.conTxt.Name = "conTxt";
+            this.conTxt.Size = new System.Drawing.Size(40, 20);
+            this.conTxt.TabIndex = 4;
+            // 
+            // intTxt
+            // 
+            this.intTxt.Location = new System.Drawing.Point(535, 50);
+            this.intTxt.Name = "intTxt";
+            this.intTxt.Size = new System.Drawing.Size(40, 20);
+            this.intTxt.TabIndex = 5;
+            // 
+            // wisTxt
+            // 
+            this.wisTxt.Location = new System.Drawing.Point(598, 51);
+            this.wisTxt.Name = "wisTxt";
+            this.wisTxt.Size = new System.Drawing.Size(40, 20);
+            this.wisTxt.TabIndex = 6;
+            // 
+            // charTxt
+            // 
+            this.charTxt.Location = new System.Drawing.Point(660, 51);
+            this.charTxt.Name = "charTxt";
+            this.charTxt.Size = new System.Drawing.Size(40, 20);
+            this.charTxt.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(349, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Strength";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(404, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Dexterity";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(458, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Constitution";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(528, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Intelligence";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(595, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Wisdom";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(655, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Charisma";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(70, 104);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Class";
+            // 
+            // randomClassBtn
+            // 
+            this.randomClassBtn.Location = new System.Drawing.Point(139, 137);
+            this.randomClassBtn.Name = "randomClassBtn";
+            this.randomClassBtn.Size = new System.Drawing.Size(75, 38);
+            this.randomClassBtn.TabIndex = 16;
+            this.randomClassBtn.Text = "Randomize Class";
+            this.randomClassBtn.UseVisualStyleBackColor = true;
+            this.randomClassBtn.Click += new System.EventHandler(this.randomClassBtn_Click);
+            // 
+            // randomStatsBtn
+            // 
+            this.randomStatsBtn.Location = new System.Drawing.Point(488, 92);
+            this.randomStatsBtn.Name = "randomStatsBtn";
+            this.randomStatsBtn.Size = new System.Drawing.Size(75, 36);
+            this.randomStatsBtn.TabIndex = 17;
+            this.randomStatsBtn.Text = "Randomize Stats";
+            this.randomStatsBtn.UseVisualStyleBackColor = true;
+            this.randomStatsBtn.Click += new System.EventHandler(this.randomStatsBtn_Click);
+            // 
+            // raceTxt
+            // 
+            this.raceTxt.Location = new System.Drawing.Point(111, 32);
+            this.raceTxt.Name = "raceTxt";
+            this.raceTxt.Size = new System.Drawing.Size(159, 20);
+            this.raceTxt.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(70, 35);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(33, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Race";
+            // 
+            // subRaceTxt
+            // 
+            this.subRaceTxt.Location = new System.Drawing.Point(111, 66);
+            this.subRaceTxt.Name = "subRaceTxt";
+            this.subRaceTxt.Size = new System.Drawing.Size(159, 20);
+            this.subRaceTxt.TabIndex = 20;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(50, 67);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Sub-Race";
+            // 
+            // featOrFlawTxt
+            // 
+            this.featOrFlawTxt.Location = new System.Drawing.Point(448, 166);
+            this.featOrFlawTxt.Multiline = true;
+            this.featOrFlawTxt.Name = "featOrFlawTxt";
+            this.featOrFlawTxt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.featOrFlawTxt.Size = new System.Drawing.Size(285, 50);
+            this.featOrFlawTxt.TabIndex = 22;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(302, 184);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(140, 13);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Your random flaw or feature:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 255);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.featOrFlawTxt);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.subRaceTxt);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.raceTxt);
+            this.Controls.Add(this.randomStatsBtn);
+            this.Controls.Add(this.randomClassBtn);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.charTxt);
+            this.Controls.Add(this.wisTxt);
+            this.Controls.Add(this.intTxt);
+            this.Controls.Add(this.conTxt);
+            this.Controls.Add(this.dexTxt);
+            this.Controls.Add(this.strTxt);
+            this.Controls.Add(this.classTxt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox classTxt;
+        private System.Windows.Forms.TextBox strTxt;
+        private System.Windows.Forms.TextBox dexTxt;
+        private System.Windows.Forms.TextBox conTxt;
+        private System.Windows.Forms.TextBox intTxt;
+        private System.Windows.Forms.TextBox wisTxt;
+        private System.Windows.Forms.TextBox charTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button randomClassBtn;
+        private System.Windows.Forms.Button randomStatsBtn;
+        private System.Windows.Forms.TextBox raceTxt;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox subRaceTxt;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox featOrFlawTxt;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DNDCharaRandomizer
+{
+    public partial class Form1 : Form
+    {
+        string[] classes = { "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard", "Artificer", "Bloodhunter"}; //14 total
+        string[] races = {"Dragonborn", "Dwarf", "Elf", "Gnome", "Half-Elf", "Halfling", "Half-Orc", "Human", "Tiefling", "Warforged", "Aarakocra", "Genasi", "Goliath", "Goblin", "Kobold", "Orc", "Tabaxi" }; //17 total
+        string[] dragSub = {"Black", "Blue", "Brass", "Bronze", "Copper", "Gold", "Green", "Red", "Silver", "White" }; //10
+        string[] dwarfSub = { "Hill", "Mountain", "Duergar", "Mark of Warding" }; //4
+        string[] elfSub = { "Eladrin", "High", "Wood", "Dark", "Sea", "Shadar-Kai", "Pallid", "Mark of Shadow", "Astral", "Avariel", "Grugach", "Bishtahar", "Vahadar" }; //13
+        string[] gnomeSub = { "Deep", "Rock", "Forest", "Mark of Scribing" }; //4
+        string[] halfelfSub = { "Regular", "Mark of Detection", "Mark of the Storm" }; //3
+        string[] halflingSub = { "Lightfoot", "Stout", "Ghostwise", "Lotusden", "Mark of Hospitality", "Mark of Healing" }; //6
+        string[] halforcSub = { "Regular", "Mark of Finding" }; //2
+        string[] humanSub = { "Regular", "Mark of Finding", "Mark of Handling", "Mark of Making", "Mark of Passage", "Mark of Sentinel" }; //6
+        string[] tieflingSub = { "Asmodeus", "Baalzebul", "Dispater", "Fierna", "Glasya", "Levistus", "Mammon", "Mephistopheles", "Zariel" }; //9
+        string[] warforgedSub = { "None" }; //1
+        string[] aarakocraSub = {"Eagle", "Macaw", "Owl", "Potoo", "Finch", "Raven", "Albatross", "Chicken", "Penguin", "Ostrich"}; //10
+        string[] genasiSub = { "Air", "Earth", "Fire", "Water" }; //4
+        string[] goliathSub = { "None"}; //1
+        string[] golbinSub = { "Traditional", "Boggart", "Gremlin", }; //3
+        string[] koboldSub = { "Burrows", "Loredrake", "Winged" }; //3
+        string[] orcSub = { "Common", "Orog", "Water", "Ur'ukia" }; //4
+        string[] tabaxiSub = { "Mountain", "Grassland", "Jungle", "Forest", "Urban" }; //5
+        string[] featOrFlaw = { 
+            "You have freakishly long toe nails and you have to try and hide them or else face potential embarrassment.", 
+            "You say you're an orphan but you actually have loving parents.", 
+            "You have 3 diamond teeth, you get to pick where they are.", 
+            "You're secretly wealthy, you have 50,000 gold stached away, but you forgot where and you need to find it.",
+            "You're convinced you can fly, but you've never tested your theory out.",
+            "You can speak any language, but only when nobody is listening.",
+            "You can secretly move 5 extra feet, but don't let the DM catch you.",
+            "You have a mountain of nose hair that you feel obligated to keep braided. Let's hope you're a dwarf!",
+            "You're a struggling alcoholic, but try to hide it as best as possible",
+            "Will get absolutely infuriated at the sight of cheese",
+            "Must say your race in every sentance, kind of like a pokemon saying their own race",
+            "You have a very small bladder, you need to take frequent bathroom breaks",
+            "You physically cannot turn left. In order to go 90 degrees left, you have to turn right 270 degrees"
+        }; //13
+
+        private readonly Random randNum = new Random();
+
+        public int RandomNumber(int min, int max)
+        {
+            return randNum.Next(min, max);
+        }
+        public int GetDiceTotal()
+        {
+            int Dice1 = 0, Dice2 = 0, Dice3 = 0;
+            Dice1 = RandomNumber(1, 7);
+            Dice2 = RandomNumber(1, 7);
+            Dice3 = RandomNumber(1, 7);
+            if (Dice1 == 1)
+            {
+                Dice1 = RandomNumber(1, 7);
+            }
+            else if (Dice2 == 1)
+            {
+                Dice2 = RandomNumber(1, 7);
+            }
+            else if (Dice3 == 1)
+            {
+                Dice3 = RandomNumber(1, 7);
+            }
+
+            int Total = Dice1 + Dice2 + Dice3;
+            return Total;
+        }
+
+        private void randomClassBtn_Click(object sender, EventArgs e)
+        {
+            raceTxt.Clear();
+            classTxt.Clear();
+            subRaceTxt.Clear();
+            featOrFlawTxt.Clear();
+
+            raceTxt.Text = races[RandomNumber(0,17)];
+            classTxt.Text = classes[RandomNumber(0, 14)];
+            featOrFlawTxt.Text = featOrFlaw[RandomNumber(0, 13)];
+
+            if (raceTxt.Text == "Dragonborn")
+            {
+                subRaceTxt.Text = dragSub[RandomNumber(0, 9)];   //adjust max num
+            }
+            else if (raceTxt.Text == "Dwarf")
+            {
+                subRaceTxt.Text = dwarfSub[RandomNumber(0, 4)];   //adjust max num
+            }
+            else if (raceTxt.Text == "Elf")
+            {
+                subRaceTxt.Text = elfSub[RandomNumber(0, 14)];   //adjust max num
+            }
+            else if (raceTxt.Text == "Gnome")
+            {
+                subRaceTxt.Text = gnomeSub[RandomNumber(0, 4)];   //adjust max num
+            }
+            else if (raceTxt.Text == "Half-Elf")
+            {
+                subRaceTxt.Text = halfelfSub[RandomNumber(0, 3)];   //adjust max num
+            }
+            else if (raceTxt.Text == "Halfling")
+            {
+                subRaceTxt.Text = halflingSub[RandomNumber(0, 6)];   //adjust max num
+            }
+            else if (raceTxt.Text == "Half-Orc")
+            {
+                subRaceTxt.Text = halforcSub[RandomNumber(0, 2)];   //adjust max num
+            }
+            else if (raceTxt.Text == "Human")
+            {
+                subRaceTxt.Text = humanSub[RandomNumber(0, 6)];   //adjust max num
+            }
+            else if (raceTxt.Text == "Tiefling")
+            {
+                subRaceTxt.Text = tieflingSub[RandomNumber(0, 9)];   //adjust max num
+            }
+            else if (raceTxt.Text == "Warforged")
+            {
+                subRaceTxt.Text = warforgedSub[RandomNumber(0, 1)];   //adjust max num
+            }
+            else if (raceTxt.Text == "Aarakocra")
+            {
+                subRaceTxt.Text = aarakocraSub[RandomNumber(0, 10)];   //adjust max num
+            }
+            else if (raceTxt.Text == "Genasi")
+            {
+                subRaceTxt.Text = genasiSub[RandomNumber(0, 4)];   //adjust max num
+            }
+            else if (raceTxt.Text == "Goliath")
+            {
+                subRaceTxt.Text = goliathSub[RandomNumber(0, 1)];   //adjust max num
+            }
+            else if (raceTxt.Text == "Goblin")
+            {
+                subRaceTxt.Text = golbinSub[RandomNumber(0, 3)];   //adjust max num
+            }
+            else if (raceTxt.Text == "Kobold")
+            {
+                subRaceTxt.Text = koboldSub[RandomNumber(0, 3)];   //adjust max num
+            }
+            else if (raceTxt.Text == "Orc")
+            {
+                subRaceTxt.Text = orcSub[RandomNumber(0, 4)];   //adjust max num
+            }
+            else if (raceTxt.Text == "Tabaxi")
+            {
+                subRaceTxt.Text = tabaxiSub[RandomNumber(0, 5)];   //adjust max num
+            }
+
+        }
+
+        private void randomStatsBtn_Click(object sender, EventArgs e)
+        {
+            strTxt.Clear();
+            dexTxt.Clear();
+            conTxt.Clear();
+            intTxt.Clear();
+            wisTxt.Clear();
+            charTxt.Clear();
+
+            strTxt.Text = GetDiceTotal().ToString();
+            dexTxt.Text = GetDiceTotal().ToString();
+            conTxt.Text = GetDiceTotal().ToString();
+            intTxt.Text = GetDiceTotal().ToString();
+            wisTxt.Text = GetDiceTotal().ToString();
+            charTxt.Text = GetDiceTotal().ToString();
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
